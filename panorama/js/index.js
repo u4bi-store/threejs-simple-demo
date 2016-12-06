a9abb15fbcf3d5777b971888f7900e2713612af5
u4bi <u4bi@u4bi.com>
2016-12-06 10:37:20 +0000

    panorama render comment

--- panorama/js/index.js
+++ panorama/js/index.js
@@ -1,7 +1,7 @@
 /* panorama index.js*/
 
 var container;
-var camera, scene, renderer;
+var camera, scene, renderer ;
 var geometry, material, mesh;
 
 var info = {
@@ -24,24 +24,24 @@ init();
 render();
 
 function init(){
-  container = document.getElementById('container');
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
+  container = document.getElementById('container'); /* 돔에 접근함*/
+  renderer = new THREE.WebGLRenderer(); /* 웹GL을 지원하는 렌더러 생성*/
+  renderer.setPixelRatio(window.devicePixelRatio); /* 현재 디스플레이 픽셀 반영함*/
+  renderer.setSize(window.innerWidth, window.innerHeight); /* 렌더링할 공간임 컨테이너란 id를 가진 di=v의 높이와 너비를 설정함*/
+  container.appendChild(renderer.domElement); /* 컨테이너에 렌더러란 돔을 주입함*/
   
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1100);
-  camera.target = new THREE.Vector3(0,0,0);
-  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1100); /* 카메라 셋업함*/
+  camera.target = new THREE.Vector3(0,0,0); /* 3d 벡터 주입 (x y z)*/
+  scene = new THREE.Scene(); /* 씬에 카메라 구성을 집어넣음*/
   
-  geometry = new THREE.SphereGeometry(500, 60, 40);
-  geometry.scale(-1, 1, 1);
+  geometry = new THREE.SphereGeometry(500, 60, 40); /* 구체형의 기하체를 만듬*/
+  geometry.scale(-1, 1, 1); /*  기하체의 스케일값 조정*/
   
-  var material_data = { map : new THREE.TextureLoader().load('images/panorama.jpg')};
-  material = new THREE.MeshBasicMaterial(material_data);
-  mesh = new THREE.Mesh(geometry, material);
+  var material_data = { map : new THREE.TextureLoader().load('images/panorama.jpg')}; /* 객체로 이미지 데이터 정보 정의*/
+  material = new THREE.MeshBasicMaterial(material_data); /* 물체에 입힐 위의 이미지를 material에 로드함 */
+  mesh = new THREE.Mesh(geometry, material); /* mesh에 geometry와 meterial란 객체를 주입하여 적용함*/
   
-  scene.add(mesh);
+  scene.add(mesh); /* 씬에 mesh 객체를 넣어줌*/
   
   document.addEventListener('mousedown', mouseDown, false);
   document.addEventListener('mousemove', mouseMove,false);
@@ -80,9 +80,10 @@ function mouseDown(e){
   info.onPointerDownLat = info.lat;
 }
 
-function render(){
+function render(){ /* 말 그대로 루프가 도는 렌더임 랜딩되는 요소들 모아놓음 정리*/
   requestAnimationFrame(render);
-
+  /* 위 함수는 우리가 브라우저 탭을 이동하거나 할때 프레임을 정지시켜준다함 브라우저가 받는 부담과 배터리를 아껴줌*/
+  
   if(info.isUserInteracting === false) info.lon += 0.1;
 
   info.lat = Math.max(-85, Math.min(85, info.lat));
@@ -93,5 +94,5 @@ function render(){
   camera.target.z = 500 * Math.sin(info.phi) * Math.sin(info.theta);
   camera.lookAt(camera.target);
 
-renderer.render(scene, camera);
+	renderer.render(scene, camera); /* 컨테이너안에 입혀진 렌더러란 돔 내부의 씬과 카메라를 렌딩시킴*/
 }
