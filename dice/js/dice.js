6e424ac79da2e840bd1f47a96c7a8003537559a3
u4bi <u4bi@u4bi.com>
2016-12-09 17:25:16 +0000

    dice render controller

--- dice/js/dice.js
+++ dice/js/dice.js
@@ -4,6 +4,19 @@ var container;
 var camera, scene, renderer;
 var dice, plane;
 
+
+var dice_info = {
+  targetPos:0,
+  targetDown:0,
+  mouseX:0,
+  mouseXDown:0
+};
+
+var win = {
+  fx:window.innerWidth / 2,
+  fy:window.innerHeight / 2
+};
+
 init();
 render();
 
@@ -14,7 +27,73 @@ function init(){
   camera.position.z = 500;
   
   scene = new THREE.Scene();
+  object_init();
   
+  renderer = new THREE.CanvasRenderer();
+  renderer.setClearColor(0xf0f0f0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+  
+  document.addEventListener('mousedown', mouseDown, false);
+  document.addEventListener('touchstart', touchStart, false);
+  document.addEventListener('touchmove', touchMove, false);
+  window.addEventListener('resize', winResize, false);
+}
+
+function winResize(){
+  win.fx = window.innerWidth / 2;
+  win.fy = window.innerHeight / 2;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function mouseDown(e){
+  event.preventDefault();
+  document.addEventListener('mousemove', mouseMove, false);
+  document.addEventListener('mouseup', mouseUp, false);
+  document.addEventListener('mouseout', mouseOut, false);
+
+  dice_info.mouseXDown = event.clientX - win.fx;
+  dice_info.targetDown = dice_info.targetPos;
+}
+function mouseMove(e){
+  dice_info.mouseX = event.clientX - win.fx;
+  dice_info.targetPos = dice_info.targetDown + (dice_info.mouseX - dice_info.mouseXDown) * 0.5;
+}
+function mouseUp(e){
+  clearMouse();
+}
+function mouseOut(e){
+  clearMouse();
+}
+function clearMouse(){
+  document.removeEventListener('mousemove', mouseMove, false);
+  document.removeEventListener('mouseup', mouseUp, false);
+  document.removeEventListener('mouseout', mouseOut, false);
+}
+
+function touchStart(e) {
+  if(e.touches.length != 1) return;
+    e.preventDefault();
+    dice_info.mouseXDown = e.touches[0].pageX - win.fx;
+    dice_info.targetDown = dice_info.targetPos;
+}
+function touchMove(e) {
+  if(e.touches.length != 1) return;
+    e.preventDefault();
+    dice_info.mouseX = e.touches[0].pageX - win.fx;
+    dice_info.targetPos = dice_info.targetDown + (dice_info.mouseX - dice_info.mouseXDown) * 0.5;
+}
+
+function render(){
+  requestAnimationFrame(render);
+  plane.rotation.y = dice.rotation.y += (dice_info.targetPos - dice.rotation.y) * 0.05;
+  renderer.render(scene, camera);	
+}
+
+function object_init(){
   var geometry = new THREE.BoxGeometry(200, 200, 200);
   var materialArray = [];
   
@@ -31,19 +110,4 @@ function init(){
   var material = new THREE.MeshBasicMaterial({ color: 0xe0e0e0, overdraw: 0.5 }); /* color 설정 빛의굴곡도 0.5*/
   plane = new THREE.Mesh(geometry, material); /* plance에 위의 두 객체를 정의함*/
   scene.add(plane); /* 씬에 plane 객체를 넣어줌*/
-
-  renderer = new THREE.CanvasRenderer();
-  renderer.setClearColor(0xf0f0f0);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
-}
-
-function render(){
-  requestAnimationFrame(render);
-  dice.rotation.x += .2;
-  dice.rotation.y += .2;
-  plane.rotation.x += .2;
-  plane.rotation.y += .2;
-  renderer.render(scene, camera);	
 }
