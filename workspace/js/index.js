32aa77ec42726f3e5e80469bf58bf05b6366ebe4
u4bi <u4bi@u4bi.com>
2017-02-23 00:46:49 +0900

    workspace webgl load

--- workspace/js/index.js
+++ workspace/js/index.js
@@ -1,6 +1,3 @@
-if (!Detector.webgl)Detector.addGetWebGLMessage();
-/* detector 모듈이 없을 시 호출 */
-
 var container;
 /* webgl이 표출할 공간 정의 */
 var camera, scene, renderer;
@@ -9,15 +6,38 @@ var camera, scene, renderer;
 */
 
 
-init(); /* 돔 생성시 초기화 구문 */
-render(); /* 스크린 랜딩구문 */
+window.onload = init; /* 돔 생성시 초기화 구문 */
 
 function init(){
-    var container = ddocument.getElementById('container');
+    var container = document.getElementById('container');
     /* 컨테이너란 아이디를 가진 돔을 참조함 */
 
+    scene = new THREE.Scene(); /* 모든 요소를 담을 씬을 생성함 */
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    /* 화면을 가리킬 카메라를 정의함 */
+
+    camera.position.x = 15;
+    camera.position.y = 16;
+    camera.position.z = 13; /* 카메라의 xyz 축을 지정함 */
+    camera.lookAt(scene.position); /* 카메라의 앵글이 씬의 좌표를 가리킴 */
+
+    renderer = new THREE.WebGLRenderer(); /* 랜더러를 WebGL로 생성함 */
+    renderer.setClearColor(0x000000, 1.0); /* 랜딩되는 배경을 깔아줄 컬러 검은색 */
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    /** 기하체를 넣은 공간
+     *
+     * 
+     * 
+     * 일단 잘되는지 테스트해보자 
+     */
+
+    container.appendChild(renderer.domElement);/* 이 정의된 렌더러를 컨테이너에 주입함 */
+
+    render(); /* 스크린 랜딩구문 */
 }
 
 function render(){
 
+    renderer.render(scene, camera); /* 씬과 카메라를 주입받은 렌더러가 돈다 */
 }
