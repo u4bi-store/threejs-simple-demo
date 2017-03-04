31e233f2c4e2cb16ecae045c60ee0b9c2bcdb4fc
u4bi <u4bi@u4bi.com>
2017-03-05 03:10:23 +0900

    css render object render

--- css3d-render/js/index.js
+++ css3d-render/js/index.js
@@ -1,10 +1,8 @@
 
-var camera, scene, renderer;
-
+var camera;
 var scene2, renderer2;
 
 var controls;
-
 var container = document.getElementById('container');
 
 init();
@@ -14,51 +12,27 @@ function init() {
 
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
     camera.position.set( 200, 200, 200 );
-
     controls = new THREE.TrackballControls( camera );
-
-    // scene = new THREE.Scene();
     scene2 = new THREE.Scene();
 
-    var material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1, side: THREE.DoubleSide } );
+    var element = document.createElement( 'div' );
+    element.innerHTML = '안녕하세요';
+    element.style.width = '100px';
+    element.style.height = '50px';
+    element.style.opacity = 0.5;
+    element.style.color = '#fff';
+    element.style.background = 'red';
 
-    //
-
-    for ( var i = 0; i < 10; i ++ ) {
-
-        var element = document.createElement( 'div' );
-        element.style.width = '100px';
-        element.style.height = '100px';
-        element.style.opacity = 0.5;
-        element.style.background = new THREE.Color( Math.random() * 0xffffff ).getStyle();
-
-        var object = new THREE.CSS3DObject( element );
-        object.position.x = Math.random() * 200 - 100;
-        object.position.y = Math.random() * 200 - 100;
-        object.position.z = Math.random() * 200 - 100;
-        object.rotation.x = Math.random();
-        object.rotation.y = Math.random();
-        object.rotation.z = Math.random();
-        object.scale.x = Math.random() + 0.5;
-        object.scale.y = Math.random() + 0.5;
-        scene2.add( object );
-
-        // var geometry = new THREE.PlaneGeometry( 100, 100 );
-        // var mesh = new THREE.Mesh( geometry, material );
-        // mesh.position.copy( object.position );
-        // mesh.rotation.copy( object.rotation );
-        // mesh.scale.copy( object.scale );
-        // scene.add( mesh );
-
-    }
-
-    //
-
-    // renderer = new THREE.WebGLRenderer();
-    // renderer.setClearColor( 0xf0f0f0 );
-    // renderer.setPixelRatio( window.devicePixelRatio );
-    // renderer.setSize( window.innerWidth, window.innerHeight );
-    // container.appendChild( renderer.domElement );
+    var object = new THREE.CSS3DObject( element );
+    object.position.x = 0;
+    object.position.y = 0;
+    object.position.z = 0;
+    object.rotation.x = 0;
+    object.rotation.y = 0;
+    object.rotation.z = 0;
+    object.scale.x = 1;
+    object.scale.y = 1;
+    scene2.add( object );
 
     renderer2 = new THREE.CSS3DRenderer();
     renderer2.setSize( window.innerWidth, window.innerHeight );
@@ -69,12 +43,8 @@ function init() {
 }
 
 function animate() {
-
     requestAnimationFrame( animate );
-
     controls.update();
-
-    // renderer.render( scene, camera );
     renderer2.render( scene2, camera );
 
 }
