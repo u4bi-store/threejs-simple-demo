df947e58f069883ba5cd4647361f00131715dc67
u4bi <u4bi@u4bi.com>
2017-03-05 12:52:39 +0900

    css division 3dRender

--- css3d-render/js/index.js
+++ css3d-render/js/index.js
@@ -1,20 +1,22 @@
 
 var camera;
-var scene2, renderer2;
+var scene, renderer;
 
-var controls;
+var root;
+
 var container = document.getElementById('container');
 
 init();
-animate();
+render();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( 200, 200, 200 );
-    controls = new THREE.TrackballControls( camera );
-    scene2 = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(0,0,200);
 
+    scene = new THREE.Scene();
+
+    root = new THREE.Object3D();
     var element = document.createElement( 'div' );
     element.innerHTML = '안녕하세요';
     element.style.width = '100px';
@@ -23,28 +25,17 @@ function init() {
     element.style.color = '#fff';
     element.style.background = 'red';
 
-    var object = new THREE.CSS3DObject( element );
-    object.position.x = 0;
-    object.position.y = 0;
-    object.position.z = 0;
-    object.rotation.x = 0;
-    object.rotation.y = 0;
-    object.rotation.z = 0;
-    object.scale.x = 1;
-    object.scale.y = 1;
-    scene2.add( object );
+    var object = new THREE.CSS3DObject(element);
+    
+    root.add(object);
+    scene.add(root);
 
-    renderer2 = new THREE.CSS3DRenderer();
-    renderer2.setSize( window.innerWidth, window.innerHeight );
-    renderer2.domElement.style.position = 'absolute';
-    renderer2.domElement.style.top = 0;
-    container.appendChild( renderer2.domElement );
-
+    renderer = new THREE.CSS3DRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild(renderer.domElement);
 }
 
-function animate() {
-    requestAnimationFrame( animate );
-    controls.update();
-    renderer2.render( scene2, camera );
-
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
